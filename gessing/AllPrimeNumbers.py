8f78bfc999d28020dfe664e66acdc658cfa5c14f
sghenimi <soufiane.ghenimi@gmail.com>
2021-02-15 23:56:59 +0100

    nombres premiers

---
+++ gessing/AllPrimeNumbers.py
@@ -0,0 +1,17 @@
+import math
+class AllPrimeNumbers:
+    def countPrimes(self, n):
+        if n < 3:
+            return 0
+
+        arr = [0] * n
+        arr[0] = 1
+        arr[1] = 1
+        sqrt = int(math.sqrt(n))
+        for i in range(2, sqrt + 1):
+            j = 2
+            while (i * j) < n:
+                arr[i * j] = 1
+                j += 1
+
+        return len([num for num in arr if num == 0])
