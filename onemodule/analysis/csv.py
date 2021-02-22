c9c26f9de77564ebe08c769557eea62c3bcb410f
sghenimi <soufiane.ghenimi@gmail.com>
2021-02-22 12:23:37 +0100

    exception and logging

--- onemodule/analysis/csv.py
+++ onemodule/analysis/csv.py
@@ -1,15 +1,18 @@
 #! /usr/bin/env python3
 # coding: utf-8
 import os
+import logging as log
 
 
 def launch_analysis(data_file):
     directory = os.path.dirname(os.path.dirname(__file__))
     path_to_file = os.path.join(directory, "files", data_file)
-    with open(path_to_file, "r") as ff:
-        preview = ff.readline()
-
-    print("preview of csv file : {}".format(preview))
+    try:
+        with open(path_to_file, "r") as ff:
+            preview = ff.readline()
+        print("preview of csv file : {}".format(preview))
+    except FileNotFoundError as e:
+        log.warning(f"Hey File not found and you have a the message: {e}")
 
 
 def main():
--- onemodule/parity.py
+++ onemodule/parity.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python3
 #! coding utf-8
 import argparse
+
 import analysis.csv as acsv
 import analysis.xml as axml
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-e", "--extension", help="""Type of file to analyse. Is it a CSV or an XML?"""
+    )
+    return parser.parse_args()
+
+
 def main():
-    acsv.launch_analysis("current_mps.csv")
-    axml.launch_analysis("dscan.xml")
+    args = parse_arguments()
+    # import pdb; pdb.set_trace()
+    if args.extension == "csv":
+        acsv.launch_analysis("current_mps1.csv")
+    elif args.extension == "xml":
+        axml.launch_analysis("dscan.xml")
+    else:
+        print("only xml and csv ")
 
 
 if __name__ == "__main__":
