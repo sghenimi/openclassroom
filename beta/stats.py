614b760943ba2e93cdedc3f0ae262db519dd3968
sghenimi <soufiane.ghenimi@gmail.com>
2021-02-23 16:33:38 +0100

    <>

--- beta/stats.py
+++ beta/stats.py
@@ -5,13 +5,31 @@ import matplotlib.pyplot as plt
 un_panda = [100,5, 20, 80,]
 un_panda_numpy = np.array(un_panda)
 
-ages = [25, 65, 26, 26, 46, 37, 36, 36, 28, 28, 57, 37, 48, 48, 37, 28, 60,
+def cammenbert():
+    ages = [25, 65, 26, 26, 46, 37, 36, 36, 28, 28, 57, 37, 48, 48, 37, 28, 60,
        25, 65, 46, 26, 46, 37, 36, 37, 29, 58, 47, 47, 48, 48, 47, 48, 60]
 
-fig, ax = plt.subplots()
-ax.hist(ages)
-plt.show()
+    fig, ax = plt.subplots()
+    ax.hist(ages)
+    plt.show()
 
-fig, ax = plt.subplots()
-ax.hist(ages)
-plt.show()
+class MyIterator:
+    def __init__(self):
+        print("we start at 5 : ")
+        self.i = 5
+    
+    def __iter__(self):
+        print(" iter it self : ")
+        return self
+    
+    def __next__(self):
+        print("the next element : ")
+        self.i += 5
+        if self.i >= 25:
+            raise StopIteration()
+        return self.i
+
+for x in MyIterator():
+    print(x)
+
+
