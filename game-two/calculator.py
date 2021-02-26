73178732a970e325e2f01ce37d58fd9b57c5272b
sghenimi <soufiane.ghenimi@gmail.com>
2021-02-26 23:57:08 +0100

    calculator

---
+++ game-two/calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator for basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
