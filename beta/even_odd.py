1b3bce7f2095dc1abe7d12eaf7b3dbceba25e078
sghenimi <soufiane.ghenimi@gmail.com>
2021-05-07 07:48:15 +0200

    even & odd in a list of number

--- beta/even_odd.py
+++ beta/even_odd.py
@@ -1,16 +1,15 @@
-#Notes
-# https://www.hackerrank.com/
-# www.codingame.com
-# https://leetcode.com/
-# https://adventofcode.com/
-# https://the-algorithms.com/
+# For a string S, print its even-indexed and odd-indexed characters
 
 n = int(input().strip())
-    
+
+# with index
 def print_even_odd_0(s_original):
+    #even number starts at index 0 and continue bye 2 steps
+    #odd number starts at 2end item and continue bye 2 steps
     print(s_original[::2], s_original[1::2])
 
 
+#with loop and compare
 def print_even_odd_1(s_original):
     s_even = s_original[0]
     s_odd = ''
@@ -24,6 +23,12 @@ def print_even_odd_1(s_original):
 
     print(f'{s_even} {s_odd}')
 
-for i in range(n):
-    s = input()
-    print_even_odd_0(s)
+# for i in range(n):
+#     s = input()
+#     print_even_odd_3(s)
+
+# split a list  if even/odd with list comprehension
+def print_even_odd_3(s_original):
+    print([x for x in s_original if x%2 == 0], [x for x in s_original if x%2 != 0])
+
+print_even_odd_3([10, 33, 2, 5, 6, 7, 71])
