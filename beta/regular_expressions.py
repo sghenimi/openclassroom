ec9ef23a4c1f6b6786fb64e1dbe12dee60cf339b
sghenimi <soufiane.ghenimi@gmail.com>
2021-06-01 23:43:52 +0200

    update RE

--- beta/regular_expressions.py
+++ beta/regular_expressions.py
@@ -47,3 +47,43 @@ if searchObj:
    print("searchObj.group(2)) : ", searchObj.group(2))
 else:
    print("Nothing found!!")
+
+str = 'an example word:cat!!'
+is_match = re.search(r'word:\w\w\w', str)
+# If-statement after search() tests if it succeeded
+if is_match:
+  print('found', is_match.group()) ## 'found word:cat'
+else:
+  print('did not find')
+
+## Search for pattern 'iii' in string 'piiig'.
+  ## All of the pattern must match, but it may appear anywhere.
+  ## On success, match.group() is matched text.
+  match = re.search(r'iii', 'piiig') # found, match.group() == "iii"
+  match = re.search(r'igs', 'piiig') # not found, match == None
+
+  ## . = any char but \n
+  match = re.search(r'..g', 'piiig') # found, match.group() == "iig"
+
+  ## \d = digit char, \w = word char
+  match = re.search(r'\d\d\d', 'p123g') # found, match.group() == "123"
+  match = re.search(r'\w\w\w', '@@abcd!!') # found, match.group() == "abc"
+
+## i+ = one or more i's, as many as possible.
+  match = re.search(r'pi+', 'piiig') # found, match.group() == "piii"
+
+  ## Finds the first/leftmost solution, and within it drives the +
+  ## as far as possible (aka 'leftmost and largest').
+  ## In this example, note that it does not get to the second set of i's.
+  match = re.search(r'i+', 'piigiiii') # found, match.group() == "ii"
+
+  ## \s* = zero or more whitespace chars
+  ## Here look for 3 digits, possibly separated by whitespace.
+  match = re.search(r'\d\s*\d\s*\d', 'xx1 2   3xx') # found, match.group() == "1 2   3"
+  match = re.search(r'\d\s*\d\s*\d', 'xx12  3xx') # found, match.group() == "12  3"
+  match = re.search(r'\d\s*\d\s*\d', 'xx123xx') # found, match.group() == "123"
+
+  ## ^ = matches the start of string, so this fails:
+  match = re.search(r'^b\w+', 'foobar') # not found, match == None
+  ## but without the ^ it succeeds:
+  match = re.search(r'b\w+', 'foobar') # found, match.group() == "bar"
