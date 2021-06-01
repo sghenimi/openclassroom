a7d30affd0758e391f8f22a86207a203809ee0fd
sghenimi <soufiane.ghenimi@gmail.com>
2021-06-01 23:51:53 +0200

    update RE

--- beta/regular_expressions.py
+++ beta/regular_expressions.py
@@ -87,3 +87,47 @@ else:
   match = re.search(r'^b\w+', 'foobar') # not found, match == None
   ## but without the ^ it succeeds:
   match = re.search(r'b\w+', 'foobar') # found, match.group() == "bar"
+
+
+str = 'purple alice-b@google.com monkey dishwasher'
+ismatch = re.search(r'\w+@\w+', str)
+if ismatch:
+    print(ismatch.group())  ## 'b@google'
+
+match = re.search(r'[\w.-]+@[\w.-]+', str)
+if match:
+    print(match.group())  ## 'alice-b@google.com'
+
+str = 'purple alice-b@google.com monkey dishwasher'
+match = re.search(r'([\w.-]+)@([\w.-]+)', str)
+if match:
+    print(match.group())   ## 'alice-b@google.com' (the whole match)
+    print(match.group(1))  ## 'alice-b' (the username, group 1)
+    print(match.group(2))  ## 'google.com' (the host, group 2)
+
+## Suppose we have a text with many email addresses
+str = 'purple alice@google.com, blah monkey bob@abc.com blah dishwasher'
+
+## Here re.findall() returns a list of all the found email strings
+emails = re.findall(r'[\w\.-]+@[\w\.-]+', str) ## ['alice@google.com', 'bob@abc.com']
+for email in emails:
+    # do something with each found email string
+    print(email)
+
+# Open file
+#f = open('test.txt', 'r')
+# Feed the file text into findall(); it returns a list of all the found strings
+#strings = re.findall(r'some pattern', f.read())
+
+str = 'purple alice@google.com, blah monkey bob@abc.com blah dishwasher'
+tuples = re.findall(r'([\w\.-]+)@([\w\.-]+)', str)
+print (tuples)  ## [('alice', 'google.com'), ('bob', 'abc.com')]
+for tuple in tuples:
+    print (tuple[0])  ## username
+    print (tuple[1])  ## host
+
+str = 'purple alice@google.com, blah monkey bob@abc.com blah dishwasher'
+  ## re.sub(pat, replacement, str) -- returns new string with all replacements,
+  ## \1 is group(1), \2 group(2) in the replacement
+print(re.sub(r'([\w\.-]+)@([\w\.-]+)', r'\1@yo-yo-dyne.com', str))
+  ## purple alice@yo-yo-dyne.com, blah monkey bob@yo-yo-dyne.com blah dishwasher
